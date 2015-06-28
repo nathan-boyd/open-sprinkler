0eb1018a039f666adf5c22ad1291538e90e6700a
Nathan Boyd <nboyd@exacttarget.com>
2015-06-28 18:59:17 -0400

    adding ui testing

--- test/foo.js
+++ test/foo.js
@@ -1,5 +1,22 @@
 var assert = require('assert')
 
+var phantom = require('phantom')
+describe('browser', function(){
+    it('should test browsers', function(){
+        phantom.create(function (ph) {
+            ph.createPage(function (page) {
+                page.open("http://www.google.com", function (status) {
+                    console.log("opened google? ", status);
+                    page.evaluate(function () { return document.title; }, function (result) {
+                        console.log('Page title is ' + result);
+                        ph.exit();git
+                    });
+                });
+            });
+        });
+    })
+});
+
 describe('Array', function(){
     describe('#indexOf()', function(){
         it('should return -1 when the value is not present', function(){
