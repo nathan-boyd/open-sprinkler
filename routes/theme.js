f17da5cc181a67b25573e7eafb47c073fc94c448
Nathan Boyd <nboyd@exacttarget.com>
2015-07-19 20:35:10 -0400

    Added Bootstrap

--- routes/theme.js
+++ routes/theme.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'Hello' });
+  res.render('theme', { title: 'Theme' });
 });
 
 module.exports = router;
